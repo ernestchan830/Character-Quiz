16a69bd8b01fdc09dfd4c843ed586151bff8989f
ernestchan830 <aloeicecream@gmail.com>
2020-01-29 02:18:55 +0000

    Questions

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // Write your click handler and conditionals here
+$(".button").click(function(){
+    var talk = $(".talk").val();
+    var genre = $(".genre").val();
+
+    //alert(color);
+    if (talk === "Yes" && genre === "Action") {
+            $(".h1").text("Ice Bear");
+            
+    } else if (talk === "No" && genre === "Action") {
+            $(".h1").text("Atticus");
+    }
+      else if (talk === "Yes" && genre === "Rom-Com") {
+            $(".h1").text("Beemo");
+    } 
+      else if (talk === "No" && genre === "Rom-Com") {
+            $(".h1").text("Aggretsuko");
+
+}
+});
+
